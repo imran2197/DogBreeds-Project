b58f1a855c65a3881fe1932f397a02ef7bfc15f1
imran2197 <samimdimran786@gmail.com>
2021-02-21 13:16:44 +0530

    Login signup states

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/Login.css";
 import Footer from "../components/Footer";
 
 const Login = () => {
+    const [user, setUser] = useState({
+        username: "",
+        password: ""
+      });
+      const [existingUser, setExistingUser] = useState({
+        username: "",
+        password: ""
+      });
+    
+      const handleInput = e => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        setUser({
+          ...user,
+          [name]: value
+        });
+      };
+      const handleLogin = () => {
+        setExistingUser(user);
+        setUser({
+          username: "",
+          password: ""
+        });
+      };
     return(
         <>
             <div className="login-form">
-                <form action="/examples/actions/confirmation.php" method="post">
-                    <div className="avatar"><i className="material-icons">&#xE7FF;</i></div>
-                    <h4 className="modal-title">Login to Your Account</h4>
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Username" required="required" />
-                        </div>
-                        <div className="form-group">
-                            <input type="password" className="form-control" placeholder="Password" required="required" />
-                        </div>
-                        <input type="submit" className="btn btn-primary btn-block btn-lg" value="Login"></input>
-                </form> 
-            </div>
+      <form onSubmit={handleLogin}>
+        <div className="avatar">
+          <i className="material-icons">&#xE7FF;</i>
+        </div>
+        <h4 className="modal-title">Login to Your Account</h4>
+
+        <div className="form-group">
+          <input
+            type="text"
+            name="username"
+            onChange={handleInput}
+            className="form-control"
+            placeholder="Username"
+            value={user.username}
+            required="required"
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            onChange={handleInput}
+            className="form-control"
+            placeholder="Password"
+            value={user.password}
+            required="required"
+          />
+        </div>
+
+        <button
+          type="submit"
+          onClick={handleLogin}
+          className="btn btn-primary btn-block btn-lg"
+        >
+          Login
+        </button>
+      </form>
+    </div>
             <Footer />
         </>
     );
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,50 +1,129 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/SignUp.css";
 import Footer from "../components/Footer";
 
 const SignUp = () => {
+    const [userRegistration, setUserRegistration] = useState({
+        username: "",
+        phone: "",
+        email: "",
+        password: ""
+      });
+      const [users, setUsers] = useState({
+        username: "",
+        phone: "",
+        email: "",
+        password: ""
+      });
+    
+      const handleInput = e => {
+        const { name, value } = e.target;
+        setUserRegistration({
+          ...userRegistration,
+          [name]: value
+        });
+      };
+      const handleSubmit = e => {
+        e.preventDefault();
+        setUsers(userRegistration);
+        setUserRegistration({
+          username: "",
+          phone: "",
+          email: "",
+          password: ""
+        });
+      };
     return(
         <>
             <div className="signup-form">
-                <form action="/examples/actions/confirmation.php" method="post">
-                    <h2>Create Account</h2>
-                    <p className="lead">It's free and hardly takes more than 30 seconds.</p>
-                    <div className="form-group">
-                        <div className="input-group">
-                            <span className="input-group-addon"><i className="fa fa-user"></i></span>
-                            <input type="text" className="form-control" name="username" placeholder="Username" required="required" />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="input-group">
-                            <span className="input-group-addon"><i className="fa fa-paper-plane"></i></span>
-                            <input type="email" className="form-control" name="email" placeholder="Email Address" required="required" />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="input-group">
-                            <span className="input-group-addon"><i className="fa fa-lock"></i></span>
-                            <input type="text" className="form-control" name="password" placeholder="Password" required="required" />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="input-group">
-                            <span className="input-group-addon">
-                                <i className="fa fa-lock"></i>
-                                <i className="fa fa-check"></i>
-                            </span>
-                            <input type="text" className="form-control" name="confirm_password" placeholder="Confirm Password" required="required" />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-primary btn-block btn-lg">Sign Up</button>
-                    </div>
-		            <p className="small text-center">
-                        By clicking the Sign Up button, you agree to our <br/>
-                        Terms & Conditions and Privacy Policy.
-                    </p>
-                </form>
-            </div>
+      <form onSubmit={handleSubmit}>
+        <h2>Create Account</h2>
+        <p className="lead">It's free and hardly takes more than 30 seconds.</p>
+
+        <div className="form-group">
+          <div className="input-group">
+            <span className="input-group-addon">
+              <i className="fa fa-user" />
+            </span>
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              placeholder="Username"
+              onChange={handleInput}
+              value={userRegistration.username}
+              required="required"
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <div className="input-group">
+            <span className="input-group-addon">
+              <i className="fa fa-phone" />
+            </span>
+            <input
+              type="number"
+              className="form-control"
+              name="phone"
+              placeholder="Phone Number"
+              onChange={handleInput}
+              value={userRegistration.phone}
+              required="required"
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <div className="input-group">
+            <span className="input-group-addon">
+              <i className="fa fa-envelope" />
+            </span>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              placeholder="Email Address"
+              onChange={handleInput}
+              value={userRegistration.email}
+              required="required"
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <div className="input-group">
+            <span className="input-group-addon">
+              <i className="fa fa-lock" />
+            </span>
+            <input
+              type="text"
+              className="form-control"
+              name="password"
+              placeholder="Password"
+              onChange={handleInput}
+              value={userRegistration.password}
+              required="required"
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="btn btn-primary btn-block btn-lg"
+          >
+            Sign Up
+          </button>
+        </div>
+
+        <p className="small text-center">
+          By clicking the Sign Up button, you agree to our <br />
+          Terms & Conditions and Privacy Policy.
+        </p>
+      </form>
+    </div>
             <Footer />
         </>
     );
